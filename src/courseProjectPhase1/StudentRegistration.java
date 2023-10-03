8d1b94d9176499aa81addbfed8878892df723c1b
richardkyles2 <78246996+richardkyles2@users.noreply.github.com>
2023-10-02 23:01:46 -0500

    test

--- src/courseProjectPhase1/StudentRegistration.java
+++ src/courseProjectPhase1/StudentRegistration.java
@@ -1,9 +1,8 @@
 package courseProjectPhase1;
-
+// week 1 Richard Kyles
 public class StudentRegistration {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         System.out.println("Hello World");
         System.out.println();
         System.out.println("Congratulations on your first Java program");
