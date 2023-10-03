288532015f456b23f24fb1bb11dd6c05a2b71da7
richardkyles2 <78246996+richardkyles2@users.noreply.github.com>
2023-10-02 22:32:43 -0500

    testies

---
+++ src/courseProjectPhase1/StudentRegistration.java
@@ -0,0 +1,12 @@
+package courseProjectPhase1;
+
+public class StudentRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+        System.out.println();
+        System.out.println("Congratulations on your first Java program");
+	}
+
+}
